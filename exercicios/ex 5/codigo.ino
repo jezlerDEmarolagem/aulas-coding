6bc3da8b22b5b7048d39c9e96db7a8ac0f454109
jezlerDEmarolagem <99673970+jezlerDEmarolagem@users.noreply.github.com>
2022-03-25 22:24:08 -0300

    Create codigo.ino

---
+++ exercicios/ex 5/codigo.ino
@@ -0,0 +1,20 @@
+
+//
+const int buttonPin=2;
+const int ledPin=13;
+int buttonState=0;
+void setup()
+{
+  pinMode(buttonPin, OUTPUT);
+  pinMode (2,INPUT);
+}
+
+void loop()
+{
+  buttonState=digitalRead(2);
+  if(buttonState==HIGH) {
+  digitalWrite(13, HIGH);
+  // delay(1000); // Wait for 1000 millisecond(s)
+}
+}
+  
